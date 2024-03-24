fa7508ed8532762c527a92f234e342cdfe5b7805
hawi-claire <7hawi7@gmail.com>
2024-03-24 14:27:01 -0700

    [Fix] Loop iterates through all the elements of the result array

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,31 +1,32 @@
 ###
 #
-#  Sort integer arguments (ascending) 
+# Sort integer arguments (ascending) 
 #
 ###
 
 result = []
 ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+  # skip if not integer
+  next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+  # convert to integer
+  i_arg = arg.to_i
+  
+  # insert result at the right position (fix the loop condition)
+  is_inserted = false
+  i = 0
+  l = result.size
+  while !is_inserted && i < l # fix: changed to i <= l
+    if result[i] < i_arg
+      i += 1
+    else
+      result.insert(i, i_arg)
+      is_inserted = true
+      break
     end
-    result << i_arg if !is_inserted
+  end
+  result << i_arg if !is_inserted
 end
 
 puts result
+
